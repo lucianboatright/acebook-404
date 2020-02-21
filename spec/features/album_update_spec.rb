118134793731647b0ccbc81a89b31a5e8493ac47
Jakephillips55 <Jakep629@gmail.com>
2020-02-21 13:04:43 +0000

    rubocop fixings for test cov

--- spec/features/album_update_spec.rb
+++ spec/features/album_update_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.feature 'Timeline', type: :feature do
@@ -6,7 +8,7 @@ RSpec.feature 'Timeline', type: :feature do
     visit '/albums'
     click_link 'New Album'
     fill_in 'album_title', with: 'Title, test!'
-    attach_file("album[images][]", Rails.root + "public/uploads/album/images/2/cool-background.png")
+    attach_file('album[images][]', Rails.root + 'public/uploads/album/images/2/cool-background.png')
     click_button 'Create Album'
     expect(page).to have_content('Title, test!')
     click_link 'Back'
@@ -27,7 +29,7 @@ RSpec.feature 'Timeline', type: :feature do
     click_link 'Back'
     click_link 'Edit'
     fill_in 'album_title', with: 'Update, Test!'
-    attach_file("album[images][]", Rails.root + "app/jobs/application_job.rb")
+    attach_file('album[images][]', Rails.root + 'app/jobs/application_job.rb')
     click_button 'Update Album'
     expect { unprocessable_entity }.to raise_error
   end
